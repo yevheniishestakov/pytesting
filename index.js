3abbb73b821d852c6abc85cb40d7e1aa849b24bc
yevhenii <you@example.com>
2023-10-18 17:19:21 +0200

    added calculator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const calculator = require('./calculator')
 
 // Constants
 const PORT = 8080;
@@ -9,12 +10,10 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World' + calculator.square(1));
 });
 
-function square(number) {
-  return number * number;
-}
+
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
