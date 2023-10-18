0b7885e5908a9dfec27c9eb091140f6bd54f888f
yevhenii <you@example.com>
2023-10-18 16:35:04 +0200

    changed test

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 describe("GET /", () => {
   it("should return hello world", async () => {
-    const res = await request(app).get(
-);
-    expect(res.statusCode).toBe(200);
+    const res = await request(app).get();
+    expect(res.body.name).toBe("Hello world");
   });
 });
